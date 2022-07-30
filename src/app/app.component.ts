963909d6267501ed26069890b2a45aa017452090
Geordie-Castaneda <jordyorantes9@gmail.com>
2022-07-29 19:03:44 -0600

    Subiendo el primer cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', './register/register.component.scss']
 })
 export class AppComponent {
   title = 'tarea_2';
+  //Escribir codigo aquí :D
 }
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent {
+  title = 'tarea_2_registro';
+
+  //Escribir codigo aquí :D
+}
